970e72603f29f4306b2410610e74e3a47bdfef0b
Jordan Lindorff <jlindo6@wgu.edu>
2022-03-08 12:34:04 -0500

    Inital commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.c195_task1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.c195_task1 to javafx.fxml;
+    exports com.example.c195_task1;
+}
