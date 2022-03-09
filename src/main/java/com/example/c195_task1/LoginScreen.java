06b13c0bb9ba70a68402cb4931bb9213620ae4cb
MightyLindo <jlindo6@wgu.edu>
2022-03-09 12:58:06 -0500

    Login Screen added

---
+++ src/main/java/com/example/c195_task1/LoginScreen.java
@@ -0,0 +1,15 @@
+package com.example.c195_task1;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class LoginScreen {
+
+    @FXML
+    private void loginClicked() {
+        System.out.println("It worked");
+    }
+}
