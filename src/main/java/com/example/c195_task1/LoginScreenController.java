31f99f54f979cb8e8e2631539b0caee69220807b
MightyLindo <jlindo6@wgu.edu>
2022-03-09 15:02:17 -0500

    MainScreen added

--- src/main/java/com/example/c195_task1/LoginScreenController.java
+++ src/main/java/com/example/c195_task1/LoginScreenController.java
@@ -1,12 +1,8 @@
 package com.example.c195_task1;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import java.io.IOException;
 
-public class LoginScreen {
+public class LoginScreenController {
 
     @FXML
     private void loginClicked() {
