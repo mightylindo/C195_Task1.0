37b2af6344fe4ecc18c90ad5c26eb9a8e8e76c77
MightyLindo <jlindo6@wgu.edu>
2022-03-14 17:08:53 -0400

    Screen Changes work

--- src/main/java/com/example/c195_task1/LoginScreenController.java
+++ src/main/java/com/example/c195_task1/LoginScreenController.java
@@ -3,23 +3,34 @@ package com.example.c195_task1;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Node;
+import javafx.scene.control.*;
+
 
 
 import java.io.IOException;
 
 public class LoginScreenController {
 
+    /**
+     * @param actionEvent
+     * @throws IOException
+     */
+
+    //Need to add functionality that will check Username and Password against database
+
     @FXML
     public void loginClicked(ActionEvent actionEvent) throws IOException {
         System.out.println("It worked");
         //nextScreen(Stage);
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));
-        MainScreenController controller = fxmlLoader.load();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));
+        Parent root = loader.load();
+        MainScreenController controller = loader.getController();
         Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(root, 600, 400);
         stage.setTitle("C195 Task1");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/c195_task1/Main.java
+++ src/main/java/com/example/c195_task1/Main.java
@@ -6,12 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+/**
+ * @author Jordan Lindorff StudentID:001460491
+ */
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
         primaryStage.setTitle("C195 Task1");
         primaryStage.setScene(new Scene(root, 600, 400));
