021310ab3c0d243f1c3ddd82b7446f0ef3b87483
MightyLindo <jlindo6@wgu.edu>
2022-03-16 17:27:47 -0400

    add get/set functions to customers class

--- src/main/java/com/example/c195_task1/Customers.java
+++ src/main/java/com/example/c195_task1/Customers.java
@@ -15,7 +15,8 @@ public class Customers {
     private String lastUpdatedBy;
     private int divisionID;
 
-    public Customers(int customerID, String customerName, String address, String postalCode, String phone, Date createDate, String createdBy, Timestamp lastUpdate, String lastUpdatedBy, int divisionID){
+    public Customers(int customerID, String customerName, String address, String postalCode, String phone, Date createDate,
+                     String createdBy, Timestamp lastUpdate, String lastUpdatedBy, int divisionID){
         this.customerID = customerID;
         this.customerName = customerName;
         this.address = address;
@@ -27,4 +28,85 @@ public class Customers {
         this.lastUpdatedBy = lastUpdatedBy;
         this.divisionID = divisionID;
     }
+    /**
+     * @return customerID
+     */
+    public int getCustomerID(){return customerID;}
+    /**
+     * @param customerID to customerID to set
+     */
+    public void setCustomerID(int customerID){this.customerID = customerID;}
+    /**
+     * @return customerName
+     */
+    public String getCustomerName(){return customerName;}
+    /**
+     * @param customerName to customerName to set
+     */
+    public void setCustomerName(String customerName){this.customerName = customerName;}
+    /**
+     * @return address
+     */
+    public String getAddress(){return address;}
+    /**
+     * @param address to address to set
+     */
+    public void setAddress(String address){this.address = address;}
+    /**
+     * @return postalCode
+     */
+    public String getPostalCode(){return postalCode;}
+    /**
+     * @param postalCode to postalCode to set
+     */
+    public void setPostalCode(String postalCode){this.postalCode = postalCode;}
+    /**
+     * @return phone
+     */
+    public String getPhone(){return phone;}
+    /**
+     * @param phone to phone to set
+     */
+    public void setPhone(String phone){this.phone = phone;}
+    /**
+     * @return createDate
+     */
+    public Date getCreateDate(){return createDate;}
+    /**
+     * @param createDate to createDate to set
+     */
+    public void setCreateDate(Date createDate) {this.createDate = createDate;}
+    /**
+     * @return createdBy
+     */
+    public String getCreatedBy(){return createdBy;}
+    /**
+     * @param createdBy to createdBy to set
+     */
+    public void setCreatedBy(String createdBy) {this.createdBy = createdBy;}
+    /**
+     * @return lastUpdate
+     */
+    public Timestamp getLastUpdate(){return lastUpdate;}
+    /**
+     * @param lastUpdate to lastUpdate to set
+     */
+    public void setLastUpdate(Timestamp lastUpdate){this.lastUpdate = lastUpdate;}
+    /**
+     * @return lastUpdatedBy
+     */
+    public String getLastUpdatedBy(){return lastUpdatedBy;}
+    /**
+     * @param lastUpdatedBy to lastUpdatedBy to set
+     */
+    public void setLastUpdatedBy(String lastUpdatedBy) {this.lastUpdatedBy = lastUpdatedBy;}
+    /**
+     * @return divisionID
+     */
+    public int getDivisionID() {return divisionID;}
+    /**
+     * @param divisionID to divisionID to set
+     */
+    public void setDivisionID(int divisionID){this.divisionID = divisionID;}
 }
+
