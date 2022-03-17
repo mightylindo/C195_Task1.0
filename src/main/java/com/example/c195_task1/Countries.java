fabaafef997d3d86ae9c0e0b27b0b6022161fa3b
MightyLindo <jlindo6@wgu.edu>
2022-03-17 15:25:33 -0400

    added Countries class

---
+++ src/main/java/com/example/c195_task1/Countries.java
@@ -0,0 +1,22 @@
+package com.example.c195_task1;
+
+import java.security.Timestamp;
+import java.util.Date;
+
+public class Countries {
+    private int countryID;
+    private String country;
+    private Date createDate;
+    private String createdBy;
+    private Timestamp lastUpdated;
+    private String lastUpdatedBy;
+
+    public Countries(int countryID, String country, Date createDate, String createdBy, Timestamp lastUpdated, String lastUpdatedBy){
+        this.countryID = countryID;
+        this.country = country;
+        this.createDate = createDate;
+        this.createdBy = createdBy;
+        this.lastUpdated = lastUpdated;
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+}
