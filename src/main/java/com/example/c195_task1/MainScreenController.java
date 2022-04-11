8cd3d2843bb1cd97d3f69c66c1f47a5b24482970
MightyLindo <jlindo6@wgu.edu>
2022-04-11 17:33:08 -0400

    added alert for appointments within 15 minutes. Need to move it to the login screen.

--- src/main/java/com/example/c195_task1/MainScreenController.java
+++ src/main/java/com/example/c195_task1/MainScreenController.java
@@ -4,7 +4,9 @@ import DBAccess.*;
 import Model.*;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
@@ -15,11 +17,44 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Node;
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ResourceBundle;
 
-public class MainScreenController {
+public class MainScreenController implements Initializable {
     private String username;
 
     public void username(String username){this.username = username;}
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        System.out.println("Hello it is initialized!");
+        ObservableList<Appointments> alist = DBAppointments.getAppointments();
+        boolean myAptmt = true;
+        for(Appointments a : alist){
+            LocalDateTime currentTime = LocalDateTime.now();
+            LocalDateTime startTime = a.getStart();
+            long timeDifference = ChronoUnit.MINUTES.between(currentTime, startTime);
+            System.out.println(timeDifference);
+            if(timeDifference < 15 && timeDifference >= 0){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Appointment Imminent");
+                alert.setContentText("Appointment: " + a.getAppointmentID() + " starts at: " + a.getStart());
+                alert.showAndWait();
+                myAptmt = true;
+            }
+            else{
+                myAptmt = false;
+            }
+        }
+        if(myAptmt == false) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("No Appointment Imminent");
+            alert.setContentText("No Appointments within 15 Minutes.");
+            alert.showAndWait();
+        }
+    }
 
     @FXML
     public void test(ActionEvent actionEvent){
