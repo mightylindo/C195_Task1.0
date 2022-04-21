bbe0d04af9cdd5aeae1d308d72944485170c38b6
MightyLindo <jlindo6@wgu.edu>
2022-04-21 10:17:35 -0700

    added contact combobox to the appointments screen

--- src/main/java/com/example/c195_task1/Report1Controller.java
+++ src/main/java/com/example/c195_task1/Report1Controller.java
@@ -45,11 +45,6 @@ public class Report1Controller implements Initializable {
     public TableColumn typeColumn;
 
 
-    /**
-     * This lambda expression is used to count all appointments by type.
-     * It counts up each month as it gets a hit.
-     * It was necessary to use a lambda expression, so I could reset the month counters for each type.
-     */
     MonthCounterInterface lambdaCounter = (type)->{
         ObservableList<Reports> rlist = DBReports.getReport1(type);
         int rjan = 0;
